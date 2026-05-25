677b846d77b14ba3f5027bcd59289741da3cca14
Juuso Kosonen <kosonenjusu@gmail.com>
2026-05-25 04:18:04 +0300

    added error management and loging

--- src/app.rs
+++ src/app.rs
@@ -2,13 +2,21 @@ use crate::config::AppConfig;
 use crate::state::AppState;
 use axum::Router;
 use tokio::net::TcpListener;
+use tracing_subscriber::{EnvFilter, fmt};
 
 pub async fn build_router(state: AppState) -> Router {
     Router::new().with_state(state)
 }
 
-pub async fn build_service() -> (Router, TcpListener) {
+pub async fn create_app() -> (Router, TcpListener) {
     dotenv::dotenv().ok();
+
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .json() // comment out to get text output
+        .with_span_events(fmt::format::FmtSpan::FULL) // optional: include enter/exit span events
+        .init();
 
     let config = AppConfig::from_env();
     let state = AppState::new(&config).await;
