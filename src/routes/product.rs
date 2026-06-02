b1888ab8d10cad5d446157411bc2910baa3f307c
Juuso Kosonen <kosonenjusu@gmail.com>
2026-06-02 11:45:15 +0300

    added loggin to products route

--- src/routes/product.rs
+++ src/routes/product.rs
@@ -48,34 +48,62 @@ struct PurchaseResponse {
 
 pub fn routes() -> Router<AppState> {
     Router::new()
-        .route("/search", get(search))
         .route("/", get(get_products))
+        .route("/search", get(search))
         .route("/{barcode}", get(get_product_by_barcode))
         .route("/{barcode}/return", post(return_product))
         .route("/{barcode}/purchase", post(purchase_product))
 }
 
-async fn search(State(state): State<AppState>, body: Json<SearchRequest>) -> impl IntoResponse {
+async fn search(
+    State(state): State<AppState>,
+    Extension(auth): Extension<AuthUser>,
+    body: Json<SearchRequest>,
+) -> impl IntoResponse {
     match db::product::search_products(&body.query, &state.database).await {
-        Ok(products) => Ok(Json(ProductsResponse { products })),
+        Ok(products) => {
+            tracing::info!(
+                "User whit id {} searched for products with query: {}",
+                auth.user_id,
+                body.query
+            );
+            Ok(Json(ProductsResponse { products }))
+        }
         Err(e) => Err(e),
     }
 }
 
-async fn get_products(State(state): State<AppState>) -> impl IntoResponse {
+async fn get_products(
+    State(state): State<AppState>,
+    Extension(auth): Extension<AuthUser>,
+) -> impl IntoResponse {
     match db::product::get_products(&state.database).await {
-        Ok(products) => Ok(Json(ProductsResponse { products })),
+        Ok(products) => {
+            tracing::info!("User whit id {} fetched products", auth.user_id);
+            Ok(Json(ProductsResponse { products }))
+        }
         Err(e) => Err(e),
     }
 }
 
 async fn get_product_by_barcode(
     State(state): State<AppState>,
+    Extension(auth): Extension<AuthUser>,
     Path(barcode): Path<String>,
 ) -> impl IntoResponse {
     match db::product::find_by_barcode(&barcode, &state.database).await {
-        Ok(product) => Ok(Json(ProductResponse { product })),
-        Err(e) => Err(e),
+        Ok(product) => {
+            tracing::info!("User with id {} fetched products", auth.user_id);
+            Ok(Json(ProductResponse { product }))
+        }
+        Err(_) => {
+            tracing::warn!(
+                "User whit id {} tried to fetch unknown product {}",
+                auth.user_id,
+                barcode
+            );
+            Err(AppError::NotFound("Product not found".to_string()))
+        }
     }
 }
 
@@ -87,10 +115,20 @@ async fn return_product(
     match db::product::return_purchase(&barcode, auth.user_id, &state.database).await {
         Ok(success) => {
             if success {
+                tracing::info!(
+                    "User with id {} returned product {} successfully",
+                    auth.user_id,
+                    barcode
+                );
                 return StatusCode::OK.into_response();
+            } else {
+                tracing::info!(
+                    "User with id {} attempted to return a product {} unsuccessfully",
+                    auth.user_id,
+                    barcode
+                );
+                StatusCode::FORBIDDEN.into_response()
             }
-
-            StatusCode::FORBIDDEN.into_response()
         }
         Err(e) => e.into_response(),
     }
@@ -109,7 +147,14 @@ async fn purchase_product(
 
     let product = match db::product::find_by_barcode(&barcode, &state.database).await {
         Ok(p) => p,
-        Err(_) => return Err(AppError::NotFound("Product not found".to_string())),
+        Err(_) => {
+            tracing::warn!(
+                "User {} tried to purchase unknown product {}",
+                user.username,
+                barcode
+            );
+            return Err(AppError::NotFound("Product not found".to_string()));
+        }
     };
 
     if product.sell_price <= 0 || user.saldo > product.sell_price * (body.count - 1) {
@@ -121,12 +166,25 @@ async fn purchase_product(
                 Err(e) => return Err(e),
             };
 
+        tracing::info!(
+            "User {} purchased {} x product {}",
+            user.username,
+            body.count,
+            barcode
+        );
+
         Ok(Json(PurchaseResponse {
             balance: user.saldo - body.count * product.sell_price,
             product_stock: product.stock - body.count,
             purchases: purchases,
         }))
     } else {
+        tracing::warn!(
+            "User {} tried to purchase {} x product {} but didn't have enough money.",
+            user.username,
+            body.count,
+            barcode
+        );
         Err(AppError::InsufficientFunds)
     }
 }
