68e5e348a8206a92d890d9590b29039a75790807
Jusu05 <58607999+Jusu05@users.noreply.github.com>
2026-08-11 16:57:39 +0300

    Admin history routes (#16)

--- src/routes/admin/history/purchase.rs
+++ src/routes/admin/history/purchase.rs
@@ -4,38 +4,28 @@ use axum::{
     response::IntoResponse,
     routing::get,
 };
-use serde::Serialize;
 
 use crate::{
-    db::{self, history::PurchaseHistoryData},
+    db,
     error::Result,
     middleware::auth::AuthUser,
+    routes::history::purchase::{PurchaseHistoryRecordResponse, PurchaseHistoryResponse},
     state::AppState,
 };
 
-#[derive(Serialize)]
-pub struct PurchaseHistoryResponse {
-    pub purchases: Vec<PurchaseHistoryData>,
-}
-
-#[derive(Serialize)]
-pub struct PurchaseHistoryRecordResponse {
-    pub purchase: PurchaseHistoryData,
-}
-
 pub fn routes() -> Router<AppState> {
     Router::new()
-        .route("/", get(find_users_purchase_history))
+        .route("/", get(find_purchase_history))
         .route("/{category_id}", get(find_purchase_history_record))
 }
 
-async fn find_users_purchase_history(
+async fn find_purchase_history(
     State(state): State<AppState>,
     Extension(auth): Extension<AuthUser>,
 ) -> impl IntoResponse {
     match db::history::find_purchase_history_by_user_id(auth.user_id, &state.database).await {
         Ok(purchases) => {
-            tracing::info!("User with id {} fetched purchase", auth.user_id);
+            tracing::info!("User with id {} fetched purchase as admin", auth.user_id);
             Ok(Json(PurchaseHistoryResponse { purchases }))
         }
         Err(e) => Err(e),
@@ -47,15 +37,10 @@ async fn find_purchase_history_record(
     Extension(auth): Extension<AuthUser>,
     Path(purchase_id): Path<i32>,
 ) -> Result<Json<PurchaseHistoryRecordResponse>> {
-    let purchase = db::history::find_purchase_history_by_user_id_and_purchase_id(
-        auth.user_id,
-        purchase_id,
-        &state.database,
-    )
-    .await?;
+    let purchase = db::history::find_purchase_by_id(purchase_id, &state.database).await?;
 
     tracing::info!(
-        "User with id {} queried purchase with id {}",
+        "User with id {} queried purchase with id {} as admin",
         auth.user_id,
         purchase_id
     );
